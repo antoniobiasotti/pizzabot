62752afca1313a0ff90c012d314307fed0f1ec78
Antônio Biasotti <abamorim36@gmail.com>
2025-02-03 18:50:12 -0300

    first deploy on Hugging Face Spaces

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import openai
 import json
+import gradio as gr
 from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import ChatMessage
 
 load_dotenv()
 
@@ -37,6 +40,15 @@ def getContexto():
 
     return contexto
 
+def start_chat(openai_api_key):
+    if not openai_api_key:
+        return [], "Por favor, insira sua chave da API."
+
+    # Inicializa as mensagens com uma mensagem do assistente
+    messages = [("assistant", "Fala, beleza? Como posso te ajudar? Pergunte sobre nossa pizzaria!")]
+    return messages, ""
+
+
 # Função para fazer uma pergunta ao ChatGPT com informações dinâmicas
 def pergunta_chatgpt(pergunta, contexto):
     messages = [
@@ -57,6 +69,36 @@ def pergunta_chatgpt(pergunta, contexto):
     print(f"Assistente: {resposta_content}\n")
     messages.append({"role": "assistant", "content": resposta_content})
 
+
+# Função para gerar a resposta do chatbot com contexto da pizzaria
+def chatbot_response(messages, user_input, openai_api_key, model="gpt-3.5-turbo", temperature=0.3, max_tokens=250):
+    try:
+        if not openai_api_key:
+            return messages, "", "Por favor, insira a chave da API."
+
+        # Inicializa o modelo de linguagem com a chave da API da OpenAI e parâmetros personalizados
+        llm = ChatOpenAI(
+            openai_api_key=openai_api_key,
+            model=model,          # Define o modelo
+            temperature=temperature,  # Define a temperatura
+            max_tokens=max_tokens     # Define o número máximo de tokens
+        )
+
+        # Adiciona a mensagem do usuário ao histórico
+        messages.append(("user", user_input))
+
+       # Converte as mensagens para o formato esperado e adiciona o contexto
+        conversation = [ChatMessage(role="system", content=contexto)] + [ChatMessage(role=msg[0], content=msg[1]) for msg in messages]
+
+        # Gera a resposta
+        response = llm(conversation)
+
+        # Adiciona a resposta do assistente ao histórico
+        messages.append(("assistant", response.content))
+
+        return messages, "", ""  # Retorna as mensagens atualizadas e limpa o input
+    except Exception as e:
+        return messages, "", f"Erro: {str(e)}"
 
 def main():
     if not openai.api_key:
